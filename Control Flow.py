c13d2c0f51483e9246c04c42de1e09372c7a74dd
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-10 13:55:03 +0000

    Update Control Flow.py

--- Control Flow.py
+++ Control Flow.py
@@ -1,1 +1,17 @@
+# Control flow
+# if, else, elif
+# while and for loops
 
+trafficLightState = "yellow"
+distanceFromLight = 10
+
+if trafficLightState == "green":
+    print("go")
+elif trafficLightState == "yellow" and distanceFromLight <= 15:
+  print("speed up")
+elif trafficLightState == "yellow" and distanceFromLight > 15:
+  print("slow down")
+elif trafficLightState == "red":
+  print("stop")
+else:
+  print("unknown action")
