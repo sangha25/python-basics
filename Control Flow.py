f3ba5ecd749412ea389ebee3bdd7c5560aae9a45
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-10 13:56:46 +0000

    Update Control Flow.py

--- Control Flow.py
+++ Control Flow.py
@@ -5,16 +5,16 @@
 trafficLightState = "yellow"
 distanceFromLight = 10
 
-if trafficLightState == "green":
-    print("go")
-elif trafficLightState == "yellow" and distanceFromLight <= 15:
-  print("speed up")
-elif trafficLightState == "yellow" and distanceFromLight > 15:
-  print("slow down")
-elif trafficLightState == "red":
-  print("stop")
-else:
-  print("unknown action")
+#if trafficLightState == "green":
+  #  print("go")
+#elif trafficLightState == "yellow" and distanceFromLight <= 15:
+  #print("speed up")
+#elif trafficLightState == "yellow" and distanceFromLight > 15:
+ # print("slow down")
+#elif trafficLightState == "red":
+  #print("stop")
+#else:
+#  print("unknown action")
 
 
 if trafficLightState == "green":
