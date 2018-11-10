86785fc3e094ffaabeaf458734f59093c7300bb1
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-10 13:55:52 +0000

    Update Control Flow.py

--- Control Flow.py
+++ Control Flow.py
@@ -15,3 +15,30 @@ elif trafficLightState == "red":
   print("stop")
 else:
   print("unknown action")
+
+
+if trafficLightState == "green":
+    print("go")
+elif trafficLightState == "yellow":
+    if distanceFromLight <= 15:
+        print("speed up")
+    else:
+        print("slow down")
+elif trafficLightState == "red":
+    print("stop")
+else:
+    print("unknown action")
+
+endPoint = 10
+startPoint = 3
+currentPos = startPoint
+print(currentPos)
+
+while currentPos < endPoint:
+    currentPos += 1
+    print(currentPos)
+
+numArray = [13,53,2,67,9]
+
+for num in numArray:
+    print(num * 2)
